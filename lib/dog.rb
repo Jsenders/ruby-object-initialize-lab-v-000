0bb259891b19fcabedc905fb087eb21ca8dfb1a7
Joseph Senders <jasenders@gmail.com>
2018-05-29 15:20:04 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+  def initialize(name)
+    @name = name 
+  end
+
+  def name=(name)
+    @name = name
+  end 
+
+  def name 
+    @name 
+  end
+
+  def initialize(breed)
+    @breed = mutt 
+
+    def breed=(mutt)
+      @breed = mutt
+    end
+
+    def breed 
+      @breed
+    end
+  end
